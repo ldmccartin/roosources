6053b2b4a355f2147f4301c791b7570141f12735
Luke McCartin <ldmccartin@gmail.com>
2024-07-17 23:02:12 +0200

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { capitalize } from 'lodash';
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
+
+
+import Kangaroo from './assets/kangaroo.svg';
 import { getResources } from './server';
 import type { resource } from './server';
 import './App.css'
@@ -18,16 +30,43 @@ function App() {
       }
     };
     fetchData();
-  });
+  },[]);
 
 
   return (
     <>
-      <h1>Roosources with Bookaburra</h1>
-      <div className="card">
-        {resources.map(r => (<><a href={r.url}><p>{r.name}</p></a></>))}
+      <div className='flex px-16 mt-4 items-center'>
+        <img src={Kangaroo} className='h-12 w-12 mr-4'></img>
+        <h1 className='text-4xl'>RooSources</h1>
+      </div>
+      <Separator className='mb-12 mt-4'/>
+      <div className='flex gap-16 px-16'>
+        {resources.map(r => renderCard(r.name, r.url))}
       </div>
     </>
+  )
+}
+
+function renderCard(name: string, url: string) {
+  const capitalisedName = capitalize(name);
+  return (
+    
+      <Card className='w-96'>
+        <CardHeader>
+        <a className='max-w-fit' href={url}>
+          <CardTitle>{capitalisedName}</CardTitle>
+        </a>
+        
+      </CardHeader>
+      <CardFooter className='flex-row justify-between'>
+        <p className='text-xs italic self-end'>
+          <a className='text-blue-400' href='https://github.com/ldmccartin/roosources'>Roosources</a>
+          {' '}powered by{' '}
+          <a className='text-blue-400' href='https://github.com/ldmccartin/bookaburra'>Bookaburra</a>
+        </p>
+        <Button className='max-w-fit' variant="destructive">remove</Button>
+      </CardFooter>
+      </Card>
   )
 }
 
